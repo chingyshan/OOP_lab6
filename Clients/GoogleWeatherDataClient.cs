0ebf667e5b71b96f45f751c7a0555d7c2e4517c7
Vlad_Ageichik <vlad.ag070906@gmail.com>
2026-05-11 18:37:48 +0300

    test: add failing tests for GoogleWeatherDataClient

---
+++ Clients/GoogleWeatherDataClient.cs
@@ -0,0 +1,16 @@
+﻿using Forecast.Utils;
+
+namespace Forecast.Clients;
+
+public class GoogleWeatherDataClient : IWeatherDataClient
+{
+    public GoogleWeatherDataClient(IConfiguration config, HttpClient httpClient)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal> LocationCurrentTemperature(decimal latitude, decimal longitude)
+    {
+        throw new NotImplementedException();
+    }
+}
