4933356d2d94a6eea0090fc1ead3543792126474
Vlad_Ageichik <vlad.ag070906@gmail.com>
2026-05-11 18:57:18 +0300

    test: add failing tests for WeatherProviderSelector

---
+++ Clients/WeatherProviderSelector.cs
@@ -0,0 +1,9 @@
+﻿namespace Forecast.Clients;
+
+public class WeatherProviderSelector(IWeatherDataClient openWeather, IWeatherDataClient google)
+{
+    public IWeatherDataClient GetProvider(string provider)
+    {
+        throw new NotImplementedException();
+    }
+}
