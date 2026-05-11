4039fb25ab2d772941979065c5172ca0800bb868
Vlad_Ageichik <vlad.ag070906@gmail.com>
2026-05-12 00:25:25 +0300

    test: add failing tests for multi-location temperature

--- Controllers/CurrentWeatherController.cs
+++ Controllers/CurrentWeatherController.cs
@@ -5,11 +5,14 @@ namespace Forecast.Controllers;
 
 public class CurrentWeatherController(IWeatherDataClient client)
 {
-    private readonly IWeatherDataClient client = client;
-
     public async Task<CurrentWeather> GetCurrentWeather(decimal latitude, decimal longitude)
     {
         var temperature = await client.LocationCurrentTemperature(latitude, longitude);
         return new(temperature);
+    }
+
+    public Task<List<CurrentWeather>> GetTemperaturesForLocations(List<(decimal lat, decimal lon)> locations)
+    {
+        throw new NotImplementedException();
     }
 }
